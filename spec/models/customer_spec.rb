85b735ecf603197138dc5357136c4419c3301365
vagrant <vagrant@vagrant.vm>
2021-05-20 17:02:05 -0400

    Complete Week 9

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -13,11 +13,25 @@ RSpec.describe Customer, type: :model do
     subject.last_name=nil
     expect(subject).to_not be_valid
   end
-  it "is not valid without a phone number"
-  it "is not valid without an email"
-  it "is not valid if the phone number is not 10 chars"
-  it "is not valid if the phone number is not all digits"
-  it "is not valid if the email address doesn't have a @"
+  it "is not valid without a phone number" do
+    subject.phone=nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without an email" do
+    subject.email=nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid if the phone number is not 10 chars" do
+    expect(subject.phone.length).to eql(10)
+    #expect(subject.phone.size).to eql(10)
+  end
+  it "is not valid if the phone number is not all digits" do
+    expect(subject.phone).to satisfy {|object| object = Integer}
+    #expect((subject.phone).match?(/\A-?\d+\Z/)).to eq(true)
+  end
+  it "is not valid if the email address doesn't have a @" do
+    expect(subject.email).to include("@")
+  end
   it "returns the correct full_name" do
     expect(subject.full_name).to eq("Jack Smith")
   end
